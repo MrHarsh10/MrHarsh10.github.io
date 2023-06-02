0d193a19cbc25a1f1ee3285d87ccf6cbbc8788d3
harsh <1845296099@qq.com>
2023-06-02 23:59:46 +0800

    Site updated: 2023-06-02 23:59:45

--- admin/index.php
+++ admin/index.php
@@ -5,9 +5,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>登录</title>
+    <style>
+
+    </style>
 </head>
 <body>
-    <h1>hello </h1>
-<img src="https://s2.loli.net/2023/06/02/Wjn3N7BefiFEHz8.png"/>
+    <h1>欢迎登陆后台 </h1>
+    <a></a>
+<img width="200px"src="https://s2.loli.net/2023/06/02/Wjn3N7BefiFEHz8.png"/>
+
+    <input type="text" name="spm_id_from" placeholder="Name">
+    <input type="password" name="vd_source" placeholder="密码">
+    <form action="http://www.bilibili.com/video/BV1GJ411x7h7/">    <input type="submit" value="登陆"></form>
+
+</form>
 </body>
 </html>
